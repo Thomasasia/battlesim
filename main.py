ca05d000b4ad44ba4221a09cc63929f7daf0c595
thomasasia <thomas.harriett@gmail.com>
2023-05-03 18:15:48 -0400

    ranks now populated, oh boy!

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 
-class soldier:
+class Soldier:
     name = "default"
     character = "d"
     maxhp = 1
@@ -18,8 +18,10 @@ class soldier:
     melee = True
     ranged = False
     healer = False
+    marked = False # determines if the console will output messages from this soldier especially. ie for player characters.
 
-    def __init__(self, name, character, maxhp, attack_ph, attack_m, defense_ph, defense_m, attacks, penetration, aoe, morale, moralepool, size, melee, ranged, healer):
+
+    def __init__(self, name, character, maxhp, attack_ph, attack_m, defense_ph, defense_m, attacks, penetration, aoe, morale, moralepool, size, melee, ranged, healer, marked):
         self.name = name
         self.character = character
         self.maxhp = maxhp
@@ -38,15 +40,18 @@ class soldier:
         self.melee = melee
         self.ranged = ranged
         self.healer = healer
+        self.marked = marked
 
-class regiment:
+class Regiment:
     num_ranks = 1
     rank = [] # all ranks
     soldiers = [] # all soldiers
-    def __init__(self, soldiers, num_ranks):
-        self.num_ranks = num_ranks
-        self.soliders = soldiers
-        sort_soldiers()
+
+    def swap_ranks(self, pos1, pos2):
+        temp = self.rank[pos1]
+        self.rank[pos1] = self.rank[pos2]
+        self.rank[pos2] = temp
+
     def sort_soldiers(self):
         self.rank.clear()
         for i in range(self.num_ranks):
@@ -57,38 +62,68 @@ class regiment:
                 types[soldier.character] = []                
             types[soldier.character].append(soldier)
         # sort types by count
-
         t = []
-        for key in types.keys:
+        for key in types.keys():
             if len(t) == 0:
-                t.append(name)
+                t.append(key)
             else:
                 slen = len(t)
-                for n in range(t):
+                for n in range(len(t)):
                     if len(types[key]) >= len(types[t[n]]):
                         t.insert(n, key)
                         break
-                is slen == len(t):
+                if slen == len(t):
                     t.append(key)
         # add each type into the middle
         # this will surround the commanders by their soldiers
         for key in t:
-            num_solders = len(key)
-            per_rank = num_soldiers / self.num_ranks
+            num_soldiers = len(types[key])
+            per_rank = int(num_soldiers / self.num_ranks)
             soldier_index = 0
             for r in self.rank:
                 for i in range(per_rank):
-                    r.insert(len(r)/2, key[soldier_index])
+                    r.insert(int(len(r)/2), types[key][soldier_index])
                     soldier_index+=1
             # put extras into the first rank
             while soldier_index < num_soldiers:
-                self.rank[0]..insert(len(r)/2, key[soldier_index])
+                self.rank[0].insert(int(len(r)/2), types[key][soldier_index])
                 soldier_index+=1
+            
+            # the front will have the most variety, so we move that to the back so commanders dont die immediately.
+            if self.num_ranks > 1:
+                self.swap_ranks(0, len(self.rank)-1)
+
+    def __init__(self, soldiers, num_ranks):
+        self.num_ranks = num_ranks
+        self.soldiers = soldiers
+        self.sort_soldiers()
 
 
 
 
-class army:
+
+class Army:
     regiments = []
     def __init__(self, regiments):
         self.regiments = regiments
+    def print_army(self):
+        for regiment in self.regiments:
+            print()
+            for rank in regiment.rank:
+                for soldier in rank:
+                    print(soldier.character, end='')
+                print()
+
+
+
+soldiers = []
+for i in range(100):
+    soldiers.append(Soldier("Dude", 'A', 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, True, False, False, False))
+for i in range(30):
+    soldiers.append(Soldier("Dude", 'B', 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, True, False, False, False))
+for i in range(1):
+    soldiers.append(Soldier("Dude", 'C', 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, True, False, False, False))
+
+reg = Regiment(soldiers, 3)
+army = Army([reg])
+army.print_army()
